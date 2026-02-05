704051da63ba9bb967210aa7801ab215f215b916
Tanushree Biswas <tanushreebiswas092@gmail.com>
2026-02-05 10:02:46 +0530

    05/02/26

---
+++ src/BasicsOfJava/AreaOfCircle.java
@@ -0,0 +1,10 @@
+package BasicsOfJava;
+
+public class AreaOfCircle {
+    public static void main() {
+        double r = 3;
+        double pi = 3.1415;
+        double a = pi * r * r ;
+        System.out.println("The area of the circle is: "+a);
+    }
+}
